fe20063ec17030e25ed60e81a00ed68065fda99e
Ranajit Banerjee <ranajit.113124@gmail.com>
2019-12-27 13:38:04 +0530

    - Retry failed links - Resolve relative links

--- src/index.js
+++ src/index.js
@@ -17,30 +17,47 @@ outDirPath = outDirPath || `${path.dirname(inputDir)}/out`;
 outDirPath = path.resolve(outDirPath);
 const rootDirName = path.basename(inputDir);
 let rootDirPath = inputDir;
+const pagesMap = {};
 
-const santizeElement = (elem, $) => {
+const santizeElement = (elem, filePath) => {
     const textContent = elem.text();
 
-    if (/codepen/.test(textContent)) {
+    if (/codepen/.test(textContent) && !/template/.test(textContent)) {
         const url = `https://codepen.io/api/oembed?format=json&url=${textContent}&height=${CODE_PEN_HEIGHT}`;
-        return replaceLink(url, elem);
+        return replaceLink(url, elem, filePath);
     }
+    return null;
 };
 
-const normalizeCodeBlocks = ($) => {
-    return new Promise((res, rej) => {
-        const links = $('a');
-        const promises = [];
+const fetchLinks = (links, res, retryCount, filePath) => {
+    const promises = [];
 
-        links.each(function () {
-            promises.push(santizeElement($(this)));
-        });
+    links.forEach((link) => {
+        const promise = santizeElement(link);
+        if (promise) {
+            promises.push(promise);
+        }
+    });
 
-        Promise.all(promises).then(() => {
+    Promise.all(promises).then((results) => {
+        const fails = results.filter(d => d.type === 'fail');
+        if (fails.length && retryCount > 0) {
+            // console.log(fails, results.length, filePath);
+            fetchLinks(fails.map(d => d.elem), res, retryCount - 1, filePath);
+        } else {
             res(true);
-        }).catch((errmsg) => {
-            rej(errmsg);
+        }
+    });
+};
+
+const normalizeCodeBlocks = ($, filePath) => {
+    return new Promise((res, rej) => {
+        const links = [];
+
+        $('a').each(function () {
+            links.push($(this));
         });
+        fetchLinks(links, res, 5, filePath);
     });
 };
 
@@ -52,34 +69,67 @@ const removeTableIds = ($) => {
 };
 
 const addHighLightJSResources = ($) => {
-    const { IFRAME_ASSETS_PATH ,LOCAL_CSS, LOCAL_SCRIPT} = process.env;
+    const { IFRAME_ASSETS_PATH ,LOCAL_CSS, LOCAL_SCRIPT } = process.env;
 
     const cssList = fs.readdirSync(path.resolve(LOCAL_CSS));
     const scriptList = fs.readdirSync(path.resolve(LOCAL_SCRIPT));
-  
+
     cssList.forEach(css => $('head').append(`<link rel="stylesheet" href="${IFRAME_ASSETS_PATH}/css/${css}">`));
     scriptList.forEach(js => $('head').append(`<script src="${IFRAME_ASSETS_PATH}/js/${js}"></script>`));
-    scriptList.forEach(js => $('body').after('<script>hljs.initHighlightingOnLoad();</script>'));   
-}
+    scriptList.forEach(js => $('body').after('<script>hljs.initHighlightingOnLoad();</script>'));
+};
+
+const hyphenate = (str) => {
+    return [str.substring(0, 8), str.substring(8, 12), str.substring(12, 16),
+        str.substring(16, 20), str.substring(20, 32)].join('-')
+};
+
+const resolveRelativeLinks = ($, filePath) => {
+    $('a').each(function () {
+        const href = $(this).attr('href');
+        if (/notion\.so/g.test(href)) {
+            const hash = href.substring(href.lastIndexOf('-') + 1);
+            const [id, hashStr] = hash.split('#');
+            if (pagesMap[id]) {
+                const newPath = path.dirname(filePath);
+                const relativePath = path.relative(newPath, pagesMap[id]);
+
+                const withHash = hashStr ? `#${hyphenate(hashStr)}` : '';
+                $(this).attr('href', `${relativePath}${withHash}`);
+            }
+        }
+    });
+};
 
 const sanitizeHtml = filePath => new Promise((res, rej) => {
     fs.readFile(filePath, 'utf8', (err, data) => {
         if (err) throw rej(err);
         const $ = cheerio.load(data);
-        const hasLinks = $(PAGE_LINK_CLASS);
+        const links = $(PAGE_LINK_CLASS);
+        const linkNames = {};
+        links.each(function (i) {
+            const html = $(this);
+            const id = html.attr('id');
+            const href = html.find('a').attr('href');
+            const name = path.basename(href).replace('.html', '').replace(/%20/g, ' ');
+            linkNames[name] = i;
+            pagesMap[id.replace(/-/g, '')] =  `${filePath.replace('.html', '')}/${name}.html`;
+        });
+
         removeTableIds($);
-        
+        resolveRelativeLinks($, filePath);
         addHighLightJSResources($);
-        normalizeCodeBlocks($).then(() => {
+        normalizeCodeBlocks($, filePath).then(() => {
             res({
                 html: $.html(),
-                hasLinks: !!hasLinks.length
+                hasLinks: !!links.length,
+                links: linkNames
             });
         });
     });
 });
 
-async function sanitizeFile (filePath, outDir, jsonObj) {
+async function sanitizeFile (filePath, outDir, jsonObj, linksOrder = {}, write = false) {
     const ext = path.extname(filePath);
     const dirpath = path.dirname(filePath);
     const dirname = path.basename(dirpath);
@@ -87,50 +137,51 @@ async function sanitizeFile (filePath, outDir, jsonObj) {
 
     if (ext === '.html') {
         try {
-            const { html, hasLinks } = await sanitizeHtml(filePath);
+            const { html, hasLinks, links } = await sanitizeHtml(filePath);
             const newFilePath = `${outDir}/${basename}.html`;
-            await writeFile(newFilePath, html);
+            write && await writeFile(newFilePath, html);
 
             if (rootDirName === dirname) {
                 rootDirPath = `${inputDir}/${basename}`;
-                await traverseDirectory(filePath.replace('.html', ''), jsonObj, basename);
+                await traverseDirectory(filePath.replace('.html', ''), jsonObj, links, write);
             } else {
                 if (hasLinks) {
                     jsonObj[basename] = {
                         subPages: {},
-                        path: filePath.replace(rootDirPath, '.')
+                        path: filePath.replace(rootDirPath, '.'),
+                        order: linksOrder[basename]
                     };
-                    await traverseDirectory(filePath.replace('.html', ''), jsonObj[basename].subPages, basename);
+                    await traverseDirectory(filePath.replace('.html', ''), jsonObj[basename].subPages, links, write);
                 } else {
                     if (!jsonObj[basename]) {
                         jsonObj[basename] = {
                             title: basename,
-                            path: filePath.replace(rootDirPath, '.')
+                            path: filePath.replace(rootDirPath, '.'),
+                            order: linksOrder[basename]
                         };
                     }
+                    // pagesMap[]
                 }
             }
-
-
         } catch (e) {
             throw new Error(e);
         }
     }
 }
 
-async function processFiles (files, dir, outDir, jsonObj) {
+async function processFiles (files, dir, outDir, jsonObj, linkTitles, write) {
     return await Promise.all(files.map(async file => {
         const filePath = path.resolve(dir, file);
-        return await sanitizeFile(filePath, outDir, jsonObj);
+        return await sanitizeFile(filePath, outDir, jsonObj, linkTitles, write);
     }));
 }
 
-async function traverseDirectory (dir, jsonObj = {}) {
+async function traverseDirectory (dir, jsonObj = {}, linkTitles = {}, write) {
     try {
         const files = await readdir(dir);
         const newDir = `${outDirPath}${dir.replace(`${inputDir}`, '')}`;
         await mkdir(newDir);
-        await processFiles(files, dir, newDir, jsonObj);
+        await processFiles(files, dir, newDir, jsonObj, linkTitles, write);
     }  catch (e) {
         throw new Error(e);
     }
@@ -139,12 +190,33 @@ async function traverseDirectory (dir, jsonObj = {}) {
 removeDir(outDirPath);
 
 const json = {};
-traverseDirectory(inputDir, json).then(() => {
-    const jsonStr = JSON.stringify(json, null, 4);
 
-    if (jsonStr) {
-        writeFile(`${outDirPath}/page-links.json`, jsonStr).then(() => {
-            console.log('Done');
-        });
+const recurse = (obj) => {
+    const subPages = [];
+    for (let key in obj) {
+        if (obj[key].subPages) {
+            const childPages = recurse(obj[key].subPages);
+            subPages.push({
+                title: key,
+                subPages: childPages,
+                path: obj[key].path,
+                order: obj[key].order
+            });
+        } else {
+            subPages.push(obj[key]);
+        }
     }
+    return subPages.sort((a, b) => a.order - b.order);
+};
+
+traverseDirectory(inputDir, json, {}, false).then(() => {
+    removeDir(outDirPath);
+    traverseDirectory(inputDir, json, {}, true).then(() => {
+        const jsonStr = JSON.stringify(json, null, 4);
+        if (jsonStr) {
+            writeFile(`${outDirPath}/page-links.json`, jsonStr).then(() => {
+                console.log('Done');
+            });
+        }
+    });
 });
--- src/replace-link.js
+++ src/replace-link.js
@@ -1,24 +1,37 @@
 const https = require('https');
 const cheerio = require('cheerio');
 
-module.exports = (url, elem) => new Promise((res, rej) => {
+function tryParseJSON (jsonString) {
+    try {
+        const o = JSON.parse(jsonString);
+
+        if (o && typeof o === 'object') {
+            return o;
+        }
+    }
+    catch (e) { }
+
+    return false;
+}
+
+module.exports = (url, elem, filePath) => new Promise((res, rej) => {
     https.get(url, (resp) => {
         let data = '';
         resp.on('data', (chunk) => {
             data += chunk;
         });
         resp.on('end', () => {
-            try {
+            if (tryParseJSON(data)) {
                 let { html } = JSON.parse(data);
                 const $ = cheerio.load(html);
                 const src = `${$('iframe').attr('src')}&editable=true`;
                 // $('iframe').attr('src', 'sss');
                 $('iframe').attr('src', src);
-                res($.html());
                 elem.replaceWith($.html());
-            } catch (err) {
-                console.log('Error parsing', data, url);
-                rej();
+
+                res({ type: 'success', html: $.html() });
+            } else {
+                res({ type: 'fail', elem, url });
             }
         });
     });
